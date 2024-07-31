ff5aad6a5ded5fc8c8555aed2582f1d70a1855c1
Jeonghun <ios.jhpark@gmail.com>
2024-07-31 11:39:32 +0900

    init

---
+++ js/common.js
@@ -0,0 +1,5 @@
+if (navigator.userAgent.indexOf("Trident") > 0) {
+    alert("이 사이트에 대한 Internet Explorer 지원이 종료되었습니다. " +
+        "확인 버튼을 누르면 Edge 브라우저로 이동합니다.")
+    window.location = 'microsoft-edge:' + window.location.href;
+};
